1d4f0a09902a7a323a66fc05acb63464d0117a85
Rakshitha <27rakshitha@gmail.com>
2024-05-10 16:35:00 +0530

    destructuring

--- destructuring.js
+++ destructuring.js
@@ -22,16 +22,5 @@ const hobbies = ['Reading Novels', 'Gardening', 'Painting'];
 const [hobby1,hobby2] = hobbies;
 console.log(hobby1,hobby2);
 
-const obj1 = {'key1': 1, "key2": 2, "key3": 1000}
-
-let { key1, key3} = obj1
-
-
-
-key1 = 20;
-
-key3 = 123
-
-console.log(obj1.key1, obj1.key3)
 
 
