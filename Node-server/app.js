57032ea9e5dd2496cdc7a96f7169bf6a19e1cfd9
Rakshitha <27rakshitha@gmail.com>
2024-05-14 11:42:25 +0530

    Added requests and responses

--- Node-server/app.js
+++ Node-server/app.js
@@ -2,11 +2,29 @@ const http = require('http');
 
 
 const server = http.createServer((req,res) => {
+    /*
     console.log('Rakshitha');
     res.end('Welcome to Rakshithas server ');
-    //process.exit();
+    */
+
+    //Requests
+    console.log(req.url, req.method, req.headers);
+
+    //Responses
+    const url = req.url;
+    res.setHeader('Content-Type', 'text/html');
+    if(url === '/home') {
+        res.write('<html><body><h1>Welcome home</h1></body></html>');
+    } else if (url === '/about') {
+        res.write('<html><body><h1>Welcome to About Us page!</h1></body></html>');
+    } else if (url === '/node') {
+        res.write('<html><body><h1>Welcome to my Node Js project!!</h1></body></html>');
+    } else {
+        res.write('<html><body><h1>Page not found</h1></body></html>');
+    }
+    res.end();
 });
 
 server.listen(4000, () => {
-    console.log('Server is running on port 4000')
+    console.log('Server is running on port 4000');
 });
