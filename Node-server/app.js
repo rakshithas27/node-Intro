ce4b6c3f19dbfd08f94283749517f70bdaac21ef
Rakshitha <27rakshitha@gmail.com>
2024-05-14 11:13:43 +0530

    node Server

--- Node-server/app.js
+++ Node-server/app.js
@@ -4,8 +4,9 @@ const http = require('http');
 const server = http.createServer((req,res) => {
     console.log('Rakshitha');
     res.end('Welcome to Rakshithas server ');
+    //process.exit();
 });
 
 server.listen(4000, () => {
-    console.log('Serevr is running on port 4000')
+    console.log('Server is running on port 4000')
 });
