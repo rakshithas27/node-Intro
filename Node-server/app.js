23c545ae7c86bbdb828ad98141bea789c1186e3c
Rakshitha <27rakshitha@gmail.com>
2024-05-14 16:27:20 +0530

    Moved route handling to routes.js for better organization.

--- Node-server/app.js
+++ Node-server/app.js
@@ -1,55 +1,8 @@
 const http = require('http');
-const fs = require('fs');
 
+const routes = require('./routes')
 
-const server = http.createServer((req,res) => {
-
-    const url = req.url;
-    const method = req.method;
-
-    res.setHeader('Content-Type', 'text/html');
-
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Form</title></head>');
-        res.write('<body>');
-        fs.readFile('message.txt',(err,data) => {
-            if(!err && data) {
-                res.write(`<p> ${data} </p>`);
-            }
-            res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>')
-            res.write('</body>');
-            res.write('</html>');
-            return res.end();
-        })
-    } 
-    else if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                if(err) {
-                    console.log(err);
-                    res.statusCode = 500;
-                    res.end("Error saving the message");
-                    return;
-                }
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });  
-    }  else {
-        res.statusCode = 404;
-        res.write('<html><body><h1>Page not found!</h1></body></html>');
-        res.end();
-    }   
-});
+const server = http.createServer(routes);
 
 server.listen(4000, () => {
     console.log('Server is running on port 4000');
