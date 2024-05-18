1114b5a1b6bd640ce035b5e0e8ff1a46947ffd39
Rakshitha <27rakshitha@gmail.com>
2024-05-18 13:27:14 +0530

    Added an extra input field for products

--- intro-to-express/app.js
+++ intro-to-express/app.js
@@ -6,7 +6,11 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.send(`<form action="/product" method="POST">
+    <input type="text" name="title" placeholder="Product title">
+    <input type="number" name="quantity" placeholder="Product quantity">
+    <button type="submit">Add Product</button>
+    </form>`);
 });
 
 app.post('/product', (req, res, next) => {
